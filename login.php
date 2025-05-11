db61209875f064f622eebb68a8985e130e028bf6
Franchesca-Donadillo <129740015+Franchesca-Donadillo@users.noreply.github.com>
2025-05-11 23:34:01 +0800

    login php created

---
+++ login.php
@@ -0,0 +1,38 @@
+<?php
+// Start the session
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'] ?? '';
+    $password = $_POST['password'] ?? '';
+
+
+
+// Connect to the database
+$conn = new mysqli("localhost", "root," "", "user_registration");
+
+// Connection failed
+if ($conn->connect_error){
+    die("Connection failed". $conn->connect_error)
+}
+
+$query = "SELECT * FROM users WHERE username = $username AND password = $password";
+
+
+$result = $conn->query($query);
+
+if ($result->num_rows > 0) {
+    // User found, set session variables
+    $row = $result->fetch_assoc();
+    $_SESSION['user_id'] = $row['id'];
+    $_SESSION['username'] = $row['username'];
+    $_SESSION['email'] = $row['email'];
+
+    // Redirect to the welcome page
+    echo "Login successful";
+} else {
+    // Invalid credentials
+    echo "Invalid username or password";
+
+}
+?>
